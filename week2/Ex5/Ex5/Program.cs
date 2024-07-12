ebace644569865ee20f51c35d1a16b5b1b94a4a0
Weeraphat Thiangchan <137468360+WeeraphatThiangchan@users.noreply.github.com>
2024-07-12 19:17:20 +0700

    Add files via upload

---
+++ week2/Ex5/Ex5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // ตรวจสอบปีอธิกสุรทิน
+        Console.Write("กรุณาใส่ปี ค.ศ.: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+        bool isLeapYear = IsLeapYear(year);
+
+        Console.WriteLine($"ปี {year} เป็นปีอธิกสุรทินหรือไม่: {isLeapYear}");
+
+        // คำนวณภาษีรายได้
+        Console.Write("กรุณาใส่รายได้ต่อปี (บาท): ");
+        double income = Convert.ToDouble(Console.ReadLine());
+        double tax = CalculateIncomeTax(income);
+
+        Console.WriteLine($"ภาษีที่ต้องจ่ายสำหรับรายได้ {income} บาท คือ {tax} บาท");
+    }
+
+    static bool IsLeapYear(int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    static double CalculateIncomeTax(double income)
+    {
+        if (income <= 150000)
+        {
+            return 0;
+        }
+        else if (income > 150000 && income <= 300000)
+        {
+            return (income - 150000) * 0.05;
+        }
+        else if (income > 300000 && income <= 500000)
+        {
+            return (150000 * 0.05) + ((income - 300000) * 0.10);
+        }
+        else if (income > 500000)
+        {
+            return (150000 * 0.05) + (200000 * 0.10) + ((income - 500000) * 0.15);
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException("รายได้ไม่ถูกต้อง");
+        }
+    }
+}
---
+++ week2/Ex6/Ex6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("กรุณาใส่ค่าอุณหภูมิ: ");
+        double temp = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("กรุณาใส่หน่วย (C/F/K): ");
+        char unit = Convert.ToChar(Console.ReadLine().ToUpper());
+
+        switch (unit)
+        {
+            case 'C':
+                Console.WriteLine("ฟาเรนไฮต์: " + CelsiusToFahrenheit(temp));
+                Console.WriteLine("เคลวิน: " + CelsiusToKelvin(temp));
+                break;
+            case 'F':
+                Console.WriteLine("เซลเซียส: " + FahrenheitToCelsius(temp));
+                Console.WriteLine("เคลวิน: " + FahrenheitToKelvin(temp));
+                break;
+            case 'K':
+                Console.WriteLine("เซลเซียส: " + KelvinToCelsius(temp));
+                Console.WriteLine("ฟาเรนไฮต์: " + KelvinToFahrenheit(temp));
+                break;
+            default:
+                Console.WriteLine("หน่วยไม่ถูกต้อง");
+                break;
+        }
+    }
+
+    static double CelsiusToFahrenheit(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    static double CelsiusToKelvin(double celsius)
+    {
+        return celsius + 273.15;
+    }
+
+    static double FahrenheitToCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    static double FahrenheitToKelvin(double fahrenheit)
+    {
+        return (fahrenheit + 459.67) * 5 / 9;
+    }
+
+    static double KelvinToCelsius(double kelvin)
+    {
+        return kelvin - 273.15;
+    }
+
+    static double KelvinToFahrenheit(double kelvin)
+    {
+        return (kelvin * 9 / 5) - 459.67;
+    }
+}
