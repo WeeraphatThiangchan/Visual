d810d37894d45b708a40a73370c39772b62d3bbc
Weeraphat Thiangchan <137468360+WeeraphatThiangchan@users.noreply.github.com>
2024-07-12 19:11:12 +0700

    Add files via upload

---
+++ Week1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+ using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Text;
+ using System.Threading.Tasks;
+ 
+ namespace ConsoleApplication1
+ {
+   
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            double FNumber,SNumber;
+            //double SNumber1 = SNumber;
+            Console.Write("Enter your FirstNumber = ");
+            FNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter your SecondNumber = ");
+            SNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The Sum(+) of {FNumber} and {SNumber} is " + $"{FNumber + SNumber}.");
+            Console.WriteLine($"The result of minus(-) is " + $"{FNumber - SNumber}.");
+            Console.WriteLine($"The result of star(*) is " + $"{FNumber * SNumber}.");
+            Console.WriteLine($"The result of divine(/) is " + $"{FNumber / SNumber}.");
+            Console.WriteLine($"The result of Percent(%) is " + $"{FNumber % SNumber}.");
+            //Console.WriteLine($"The result of minus of {FNumber} and {SNumber} is " + $"{FNumber *= SNumber}.");
+            Console.WriteLine($"The result of star equal (**) is " + $"{ FNumber *= SNumber}.");
+            Console.WriteLine($"The result of plusplus (++) is " + $"{FNumber = SNumber++}.");
+            Console.WriteLine($"The result of minus minus (--)  is " + $"{FNumber = --SNumber}.");
+            Console.ReadKey();
+        }    
+    }
+ }
---
+++ Week1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age, years;
+            int degree = 0;
+            string degreeInput;
+
+            Console.Write("กรุณากรอกอายุ (ตัวเลข): ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("กรุณากรอกวุฒิเป็นภาษาอังกฤษ (เช่น Bachelor degree, Master degree, Doctoral degree, Other): ");
+            degreeInput = Console.ReadLine().ToLower().Trim();
+
+            // แปลงวุฒิการศึกษาเป็นตัวเลข
+            if (degreeInput == "bachelor degree")
+            {
+                degree = 1;
+            }
+            else if (degreeInput == "master degree")
+            {
+                degree = 2;
+            }
+            else if (degreeInput == "doctoral degree")
+            {
+                degree = 3;
+            }
+            else
+            {
+                degree = 0; // วุฒิไม่ตรงกับเงื่อนไขใดๆ กำหนดเป็น 0
+            }
+
+            Console.Write("กรุณากรอกอายุการทำงาน (ตัวเลข): ");
+            years = Convert.ToInt32(Console.ReadLine());
+
+            // ตรวจสอบเกณฑ์
+            if (age >= 22 && age <= 35)
+            {
+                if (degree == 1 && years >= 0)
+                {
+                    Console.WriteLine("ไม่ผ่านเกณฑ์");
+                }
+                else if ((degree == 2 || degree == 3) && years >= 0)
+                {
+                    Console.WriteLine("ผ่านเกณฑ์");
+                }
+                else
+                {
+                    Console.WriteLine("ไม่ผ่านเกณฑ์");
+                }
+            }
+            else if (age > 35)
+            {
+                if (years > 5)
+                {
+                    Console.WriteLine("ผ่านเกณฑ์");
+                }
+                else
+                {
+                    Console.WriteLine("ไม่ผ่านเกณฑ์");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ไม่ผ่านเกณฑ์");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
